3d81e829ea2297369ed509f2f04c597beae9774a
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2023-06-29 21:10:05 +0200

    update

--- auth/issueToken.js
+++ auth/issueToken.js
@@ -2,14 +2,20 @@ const jwt = require("jsonwebtoken");
 
 const jwtSecret = process.env.JWT_SECRET;
 
-const issueToken = (user) => {
+const issueToken = (user, res) => {
   const payload = {
     id: user._id,
   };
 
   const token = jwt.sign(payload, jwtSecret);
 
-  return token;
+  return res.json({
+    status: "success",
+    code: 200,
+    data: {
+      token
+    }
+  })
 };
 
 module.exports = issueToken;
